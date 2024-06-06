42a0412ee38133ffaf18db01b9871b1293ab1ef9
Yukki (Sabrina G.) <61744561+yukki03@users.noreply.github.com>
2024-06-06 12:49:10 +0000

    Refactor test_countries to sort  response.json()

--- webapp/test_main.py
+++ webapp/test_main.py
@@ -12,7 +12,7 @@ def test_root():
 def test_countries():
     response = client.get("/countries")
     assert response.status_code == 200
-    assert sorted(response.json()) == [
-        "England", "France", "Germany", "Italy", 
-         "Japan", "Peru","Portugal", "Spain"
- ]
+    assert sorted(response.json()) == sorted([
+        "England", "France", "Germany", "Italy",
+         "Japan", "Peru", "Portugal", "Spain"
+ ])
